249d2825b09a70073592bb26b539de334402aae4
Ankush <rehalankush192@gmail.com>
2025-07-13 13:40:44 +0530

    Initial Commit

---
+++ app/page.jsx
@@ -0,0 +1,25 @@
+import { Button } from "@/components/ui/button";
+import HomeSearch from "@/components/home-search";
+
+export default function Home() {
+  return(
+    <div className="pt-20 flex flex-col">
+      {/* Hero */}
+
+      <section className="relative py-16 md:py-28 dotted-background">
+        <div className="max-w-4xl mx-auto text-center ">
+          <div className="mb-8">
+            <h1 className="text-5xl md:text-8xl mb-4 gradient-title">
+              Find you dream car with MotorNest AI</h1>
+            <p className="text-xl text-gray-500 mb-8 max-w-2xl mx-auto">
+              Advanced AI car search and testdrive from thousands of vehicels.
+            </p>
+          </div>
+
+          {/* search */}
+          <HomeSearch/>
+        </div>
+      </section>
+    </div>
+  )
+}
